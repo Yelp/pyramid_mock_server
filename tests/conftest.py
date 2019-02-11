1dbd765cdc04921dc20fde59dde22687b87e0d6b
Samuele Maci <macisamuele@gmail.com>
2019-02-11 14:38:04 +0100

    Use 'old' pyramid_swagger configurations for request/response validation

--- tests/conftest.py
+++ tests/conftest.py
@@ -46,8 +46,8 @@ def _create_application(
         # pyramid_swagger config
         'pyramid_swagger.schema_directory': schema_directory,
         'pyramid_swagger.swagger_versions': ['2.0'],
-        'bravado_core.validate_requests': True,
-        'bravado_core.validate_responses': True,
+        'pyramid_swagger.enable_request_validation': True,
+        'pyramid_swagger.enable_response_validation': True,
 
         # pyramid_mock_server config
         'pyramid_mock_server.custom_view_packages': packages,
